fc8350c219a8577d72438407c1e5e6d098e4ebff
Md Zubair <101566430+MDZUBAIR599@users.noreply.github.com>
2022-06-05 22:43:40 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import AllRoutes from "./pages/AllRoutes";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      hello
+      <AllRoutes />
+    </div>
+  );
+}
